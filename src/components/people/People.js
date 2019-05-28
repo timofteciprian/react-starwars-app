8133140bc359fc64ec9ee365a1821fe13bdf4f2c
Unknown <timofteca@gmail.com>
2019-05-28 11:05:40 +0300

    added item detail

--- src/components/people/People.js
+++ src/components/people/People.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "antd/dist/antd.css";
+//import "./index.css";
 
 export default class People extends React.Component {
   state = {
     list: []
   };
 
-  async componentDidMount() {
-    const response = await fetch("https://swapi.co/api/people");
-    const data = await response.json();
-    this.setState({ list: data.results });
+  componentDidMount() {
+    fetch("https://swapi.co/api/people")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ list: json.results });
+      });
   }
 
   render() {
-    return this.state.list.map(item => <div>{item.name}</div>);
+    return this.state.list.map((item, index) => (
+      <nav>
+        <Link to={`/people/${index + 1}`}>{item.name}</Link>
+      </nav>
+    ));
   }
 }
